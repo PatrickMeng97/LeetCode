3540848bc85b4cffc071f67897e4ef4dd1779119
PatrickMeng97 <1795735765@qq.com>
2020-08-26 00:00:03 +0800

    Revert "Revert "盛最多水的容器""
    This reverts commit eae6a39d856bb60ebe41aaef2c088362c5988957.

---
+++ container_with_most_water/container_with_most_water/container_with_most_water.h
@@ -0,0 +1,14 @@
+#ifndef CONTAINER_WITH_MOST_WATER
+#define CONTAINER_WITH_MOST_WATER
+
+#include<vector>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea1(vector<int>& height);
+    int maxArea2(vector<int>& height);
+};
+
+#endif // !CONTAINER_WITH_MOST_WATER
---
+++ two_sum/two_sum/two_sum.h
@@ -0,0 +1,11 @@
+#ifndef TWO_SUM
+#define TWO_SUM
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target);
+};
+#endif // !2SUM
